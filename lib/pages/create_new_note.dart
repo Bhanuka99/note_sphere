205b3f6b7ba5cf32854140b84843ef1a99578595
Bhanuka99 <bhanuka.ariyarathne@gmail.com>
2024-10-25 11:04:55 +0530

    note form

--- lib/pages/create_new_note.dart
+++ lib/pages/create_new_note.dart
@@ -1,4 +1,9 @@
 import 'package:flutter/material.dart';
+import 'package:google_fonts/google_fonts.dart';
+import 'package:notesphere/services/note_service.dart';
+import 'package:notesphere/utils/colors.dart';
+import 'package:notesphere/utils/constants.dart';
+import 'package:notesphere/utils/text_styles.dart';
 
 class CreateNewNote extends StatefulWidget {
   
@@ -10,12 +15,186 @@ class CreateNewNote extends StatefulWidget {
 }
 
 class _CreateNewNoteState extends State<CreateNewNote> {
+
+  List<String> categories = [];
+  final NoteService noteService = NoteService();
+
+  Future _loadCategories () async {
+    categories = await noteService.getAllCategories();
+    setState(() {});
+  }
+  @override
+  void initState() {
+    _loadCategories();
+    super.initState();
+  }
+
   @override
   Widget build(BuildContext context) {
     return Scaffold(
       appBar: AppBar(
-        title: Text(
-          widget.isNewCategory.toString(),
+        title: const Text(
+          "Create a Note",
+          style: AppTextStyles.appSubtitle,
+        ),
+      ),
+      body: SingleChildScrollView(
+        child: Column(
+          children: [
+            const SizedBox(
+              height: 30,
+            ),
+            Container(
+              margin: const EdgeInsets.symmetric(
+                horizontal: AppConstants.kDefaultPadding*0.5,
+              ),
+              child: Form(
+                child: Column(
+                  children: [
+                    widget.isNewCategory ? 
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: TextFormField(
+                        style: const TextStyle(
+                          color: AppColors.kWhiteColor,
+                          fontSize: 20,
+                          fontWeight: FontWeight.w500
+                        ),
+                        decoration: InputDecoration(
+                          hintText: "New Category",
+                          hintStyle: TextStyle(
+                            color: AppColors.kWhiteColor.withOpacity(0.5),
+                            fontWeight: FontWeight.w500,
+                            fontSize: 20,
+                            fontFamily: GoogleFonts.dmSans().fontFamily,
+                          ),
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(
+                              color: AppColors.kWhiteColor.withOpacity(0.1),
+                              width: 2,
+                            )
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: const BorderSide(
+                              color: AppColors.kWhiteColor,
+                              width: 2
+                            )
+                          )
+                        ),
+                      ),
+                    ):
+                     //drop down
+                    Container(
+                      decoration: BoxDecoration(
+                        borderRadius: BorderRadius.circular(10),
+                      ),
+                      child: DropdownButtonFormField(
+                        style: TextStyle(
+                          color: AppColors.kWhiteColor,
+                          fontFamily: GoogleFonts.dmSans().fontFamily,
+                          fontWeight: FontWeight.w500,
+                          fontSize: 20
+                        ),
+                        isExpanded: false,
+                        hint: const Text("Category"),
+                        decoration: InputDecoration(
+                          enabledBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(20),
+                            borderSide: BorderSide(
+                              color: AppColors.kWhiteColor.withOpacity(0.1),
+                            width: 2
+                            ),
+                          ),
+                          focusedBorder: OutlineInputBorder(
+                            borderRadius: BorderRadius.circular(
+                              AppConstants.kDefaultPadding
+                            ),
+                            borderSide: const BorderSide(
+                              color: AppColors.kWhiteColor,
+                            width: 1
+                            ),
+                          ),
+                        ),
+                        items: categories.map((String category) {
+                          return DropdownMenuItem<String>(
+                            alignment: Alignment.centerLeft,
+                            value: category,
+                            child: Text(category,
+                            style: AppTextStyles.appButton,
+                            ),
+                          );
+                        }).toList(), 
+                        onChanged: (value){}
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    //title field
+                    TextFormField(
+                      maxLines: 2,
+                      style: const TextStyle(
+                        color: AppColors.kWhiteColor,
+                        fontSize: 30
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Note Title",
+                        hintStyle: TextStyle(
+                          color: AppColors.kWhiteColor.withOpacity(0.5),
+                          fontSize: 35,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    //content
+                    TextFormField(
+                      maxLines: 12,
+                      style: const TextStyle(
+                        color: AppColors.kWhiteColor,
+                        fontSize: 20
+                      ),
+                      decoration: InputDecoration(
+                        hintText: "Note Content",
+                        hintStyle: TextStyle(
+                          color: AppColors.kWhiteColor.withOpacity(0.5),
+                          fontSize: 20,
+                        ),
+                        border: InputBorder.none,
+                      ),
+                    ),
+                    const SizedBox(height: 10,),
+                    //button
+                    Divider(
+                      color: AppColors.kWhiteColor.withOpacity(0.2),
+                      thickness: 1,
+                    ),
+                    const SizedBox(height: 10,),
+                    Row(
+                      mainAxisAlignment: MainAxisAlignment.end,
+                      children: [
+                        ElevatedButton(
+                          style: const ButtonStyle(
+                            backgroundColor: WidgetStatePropertyAll((AppColors.kFabColor)),
+                          ),
+                          onPressed: (){}, 
+                          child: const Padding(
+                            padding: EdgeInsets.all(10),
+                            child: Text(
+                              "Save Note",
+                              style: AppTextStyles.appButton,
+                            ),
+                          )
+                        ),
+                      ],
+                    ),
+                  ],
+                ),
+              ),
+            ),
+          ],
         ),
       ),
     );
